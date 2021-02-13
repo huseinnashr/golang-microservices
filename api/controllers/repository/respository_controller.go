7d2be86d1179e76a95d57a336d57c99ac1c39a72
Husein <Muhammad.Nashr@xapiens.id>
2021-02-13 17:05:24 +0700

    repository service and controller

---
+++ api/controllers/repository/respository_controller.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/nvdhunter/golang-microservices/domain/repository"
+	"github.com/nvdhunter/golang-microservices/services"
+	"github.com/nvdhunter/golang-microservices/utils/errors"
+)
+
+func CreateRepo(c *gin.Context) {
+	var request repository.CreateRepoRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		apiErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(apiErr.Status(), apiErr)
+		return
+	}
+
+	result, err := services.RepositoryService.CreateRepo(request)
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
+}
