b0b26d9ec2f909b27b792afda01ca5cafa42bc6b
Husein <Muhammad.Nashr@xapiens.id>
2021-02-13 17:06:42 +0700

    health check endpoint

--- api/app/url_mappings.go
+++ api/app/url_mappings.go
@@ -2,8 +2,10 @@ package app
 
 import (
 	"github.com/nvdhunter/golang-microservices/controllers/repository"
+	"github.com/nvdhunter/golang-microservices/controllers/repository/polo"
 )
 
 func mapUrls() {
+	router.GET("/marco", polo.Polo)
 	router.POST("/repositories", repository.CreateRepo)
 }
